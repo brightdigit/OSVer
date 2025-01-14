33edb83c0776d615f9fb98a364b969ca679594c3
leogdion <leogdion@brightdigit.com>
2025-01-14 14:38:02 -0500

    Switching Linting to Ubuntu (#3)

--- Scripts/lint.sh
+++ Scripts/lint.sh
@@ -1,26 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 set -e  # Exit on any error
 
+# Detect OS and set paths accordingly
+if [ "$(uname)" = "Darwin" ]; then
+    DEFAULT_MINT_PATH="/opt/homebrew/bin/mint"
+elif [ "$(uname)" = "Linux" ] && [ -n "$GITHUB_ACTIONS" ]; then
+    DEFAULT_MINT_PATH="$GITHUB_WORKSPACE/Mint/.mint/bin/mint"
+elif [ "$(uname)" = "Linux" ]; then
+    DEFAULT_MINT_PATH="/usr/local/bin/mint"
+else
+    echo "Unsupported operating system"
+    exit 1
+fi
+
+# More portable way to get script directory
 if [ -z "$SRCROOT" ]; then
-    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
     PACKAGE_DIR="${SCRIPT_DIR}/.."
 else
     PACKAGE_DIR="${SRCROOT}"     
 fi
 
-if [ -z "$GITHUB_ACTION" ]; then
-    MINT_CMD="/opt/homebrew/bin/mint"
-else
-    MINT_CMD="mint"
-fi
+# Use environment MINT_CMD if set, otherwise use default path
+MINT_CMD=${MINT_CMD:-$DEFAULT_MINT_PATH}
 
 export MINT_PATH="$PACKAGE_DIR/.mint"
 MINT_ARGS="-n -m $PACKAGE_DIR/Mintfile --silent"
 MINT_RUN="$MINT_CMD run $MINT_ARGS"
 
-if [ "$LINT_MODE" == "NONE" ]; then
+if [ "$LINT_MODE" = "NONE" ]; then
     exit
-elif [ "$LINT_MODE" == "STRICT" ]; then
+elif [ "$LINT_MODE" = "STRICT" ]; then
     SWIFTFORMAT_OPTIONS=""
     SWIFTLINT_OPTIONS="--strict"
 else 
@@ -28,8 +38,7 @@ else
     SWIFTLINT_OPTIONS=""
 fi
 
-pushd $PACKAGE_DIR || exit 1
-
+pushd "$PACKAGE_DIR" || exit 1
 $MINT_CMD bootstrap -m Mintfile || exit 1
 
 if [ -z "$CI" ]; then
